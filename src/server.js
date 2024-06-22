b4724e61a28f03c9e7bcd156d92a5ce6b75dbd22
shiv-000 <151066024+shiv-000@users.noreply.github.com>
2024-06-23 01:14:13 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
+// const { app } = require(".");
+// const { connectDb } = require("./config/db");
+
+// const PORT=5000;
+// app.listen(PORT,async ()=>{
+//     await connectDb()
+//     console.log("ecommerce api listing on port ",PORT)
+// })
 const { app } = require(".");
 const { connectDb } = require("./config/db");
 
-const PORT=5000;
-app.listen(PORT,async ()=>{
-    await connectDb()
-    console.log("ecommerce api listing on port ",PORT)
-})
+const PORT = process.env.PORT || 5454;
+
+const startServer = async () => {
+  try {
+    await connectDb();
+    console.log("Database connected successfully.");
+
+    app.listen(PORT, () => {
+      console.log(`Ecommerce API listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database", error);
+    process.exit(1); // Exit the process with an error code
+  }
+};
+
+startServer();
 
